d758b82c1bee1978abf9b28c4f169251c49e0129
farizrahman4u <farizrahman4u@gmail.com>
2018-09-05 15:26:54 +0530

    uberjar

---
+++ datavec/datavec-uberjar/build.bat
@@ -0,0 +1,3 @@
+mvn -Pnative-snapshots -Puberjar -Pspark1-2.1.0 clean install -DskipTests
+#mvn -Pnative-snapshots -Puberjar -Pspark1-2.1.1 clean install -DskipTests
+#mvn -Pnative-snapshots -Puberjar -Pspark2-2.1.1 clean install -DskipTests
